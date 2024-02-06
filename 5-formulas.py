2993416986f4c274cf032f77ccf2bbf71c36a483
Wellington Lima <wellington.lima98@hotmail.com>
2024-02-06 00:08:10 -0300

    Update 5-formulas.py

--- 5-formulas.py
+++ 5-formulas.py
@@ -1,5 +1,5 @@
 from openpyxl import load_workbook
-from openpyxl.chart import BarChart, Reference
+from openpyxl.utils import get_column_letter
 
 #1- Lê pasta de trabalho e planilha
 wb = load_workbook("data/barchart.xlsx")
@@ -12,7 +12,11 @@ min_row = wb.active.min_row
 max_row = wb.active.max_row
 
 #3 -Incluindo Fórmula
-sheet["B6"] = "=SUM(B2:B5)"
-sheet["B6"].style = "Currency"
+# sheet["B6"] = "=SUM(B2:B5)"
+# sheet["B6"].style = "Currency"
+for i in range(min_column+1, max_column+1):
+    letter = get_column_letter(i)
+    sheet[f"{letter}{max_row+1}"] = f"=SUM({letter}{min_row+1}:{letter}{max_row})"
+    sheet[f"{letter}{max_row+1}"].style = "Currency"
 
 wb.save("test.xlsx")
